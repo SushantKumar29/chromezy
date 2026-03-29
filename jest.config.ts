7782eb7b8153b34d9d7089d33c94345f6332a22e
“SushantKumar29” <sushantkmr29@gmail.com>
2026-03-29 14:46:14 +0530

    fix: Update jest config to use swc/jest

--- jest.config.ts
+++ jest.config.ts
@@ -2,18 +2,19 @@ import type { Config } from "jest";
 import nextJest from "next/jest.js";
 
 const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: "./",
 });
 
-// Add any custom config to be passed to Jest
 const config: Config = {
   coverageProvider: "v8",
   testEnvironment: "jest-environment-jsdom",
+  // Jest is uses a SWC transformer that doesn't exist in our Next.js version. This is a common issue with Next.js 16 and Jest
+  transform: {
+    "^.+\\.(t|j)sx?$": "@swc/jest",
+  },
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/$1",
   },
 };
 
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 export default createJestConfig(config);
